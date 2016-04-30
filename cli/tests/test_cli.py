d3b45001a45838f4247e40f8e8ac865757813ede
Adam Dangoor <adamdangoor@gmail.com>
2016-04-30 20:42:10 +0100

    TODOs in cli test

--- cli/tests/test_cli.py
+++ cli/tests/test_cli.py
@@ -16,8 +16,11 @@ class TestCreate(object):
         """
         It is possible to start a Task from the CLI.
         """
+        # OS change directory to tmpdir
         runner = CliRunner()
         image_url = 'file:///vagrant/tasker/tests/rootfs.tar'
-        passed = ['create', image_url, 'echo 1']
+        passed = ['create', image_url, 'sleep 10000']
         result = runner.invoke(cli, passed)
+        # Assert that the sleep comand is there
+        # kill the sleep command.
         assert result == 0
