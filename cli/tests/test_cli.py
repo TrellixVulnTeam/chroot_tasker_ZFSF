497c689eb11637ea442908b9ec022e66da5c0a89
Adam Dangoor <adamdangoor@gmail.com>
2016-04-30 20:23:44 +0100

    Docstrings for CLI tests

--- cli/tests/test_cli.py
+++ cli/tests/test_cli.py
@@ -9,9 +9,13 @@ from cli import cli
 
 class TestCreate(object):
     """
+    Tests for creating a Task from the CLI.
     """
 
     def test_create_task(self):
+        """
+        It is possible to start a Task from the CLI.
+        """
         runner = CliRunner()
         passed = [
             'createa',
