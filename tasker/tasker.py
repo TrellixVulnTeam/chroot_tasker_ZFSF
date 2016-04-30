bcc86e0cae74b0d99eb0d56037f85002397514a9
Adam Dangoor <adamdangoor@gmail.com>
2016-04-30 13:40:47 +0100

    Stub for _run_chroot_process

--- tasker/tasker.py
+++ tasker/tasker.py
@@ -35,3 +35,16 @@ def _create_filesystem_dir(image_url, parent):
 
     tar_file.unlink()
     return filesystem_path
+
+
+def _run_chroot_process(filesystem, args):
+    """
+    Create a chroot jail and run a process in it.
+
+    :param pathlib.Path filesystem: The directory which should be the root of
+        the new process.
+    :param list args: List of strings. See ``subprocess.Popen.args``.
+
+    :return: ``None``.
+    """
+    pass
