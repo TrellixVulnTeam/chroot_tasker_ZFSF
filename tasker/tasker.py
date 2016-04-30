72b725d1e4d77701a71faf4a3962facbed6ee315
Adam Dangoor <adamdangoor@gmail.com>
2016-04-30 09:48:48 +0100

    Stub for task class

---
+++ tasker/tasker.py
@@ -0,0 +1,17 @@
+"""
+Create and interact with tasks in a chroot jail.
+"""
+
+
+class Task(object):
+    """
+    A process in a chroot jail.
+    """
+
+    def __init__(self):
+        """
+        Create a new task.
+
+        :rtype: None
+        """
+        pass
