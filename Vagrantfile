fa323b40c6213371c9a3f25075403516ab20d9aa
Adam Dangoor <adamdangoor@gmail.com>
2016-04-30 15:04:09 +0100

    Start of installing virtualenvwrapper in Vagrant VM by default

--- Vagrantfile
+++ Vagrantfile
@@ -9,4 +9,5 @@ Vagrant.configure(2) do |config|
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://atlas.hashicorp.com/search.
   config.vm.box = "ubuntu/trusty64"
+  config.vm.provision :shell, path: "bootstrap.sh"
 end
---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+apt-get -y install python-virtualenv
+pip install virtualenvwrapper
+cat > /home/vagrant/.bashrc << "EOF"
+export WORKON_HOME=/home/vagrant/.virtualenvs
+mkdir -p $WORKON_HOME
+source /usr/local/bin/virtualenvwrapper.sh
+EOF
+source /home/vagrant/.bashrc
