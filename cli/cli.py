b0b8cd67254556e77713f8527b7563e48dec595f
Adam Dangoor <adamdangoor@gmail.com>
2016-05-10 12:14:29 +0100

    Param docs for CLI create

--- cli/cli.py
+++ cli/cli.py
@@ -4,6 +4,7 @@ CLI for creating and interacting with tasks.
 
 import os
 import pathlib
+import shlex
 
 import click
 
@@ -24,10 +25,14 @@ def cli():
 def create(image_url, args):
     """
     Create a ``Task``.
+
+    :param str image_url: The URL (or URI) pointing to an image to download,
+        extract and use as the root of a new process.
+    :param str args: Commands to run as a new process.
     """
     task = Task(
         image_url=image_url,
-        args=args.split(),
+        args=shlex.split(args),
         download_path=pathlib.Path(os.getcwd()),
     )
     print(task.process.pid)
